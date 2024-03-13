969835d60e48f7e6d1247d1c5d6d705176bffbed
Pablo-Castillo-Mesa <pablo.castillo-mesa.ext@inetum.com>
2024-03-13 11:46:23 +0100

    WebScrapping V1.5

--- WebScrapping/web_scrapping/main.py
+++ WebScrapping/web_scrapping/main.py
@@ -26,73 +26,55 @@ def remove_tags(html):
 
 # Función que recibe los parametros a enviar en formato JSON y busca por id si el jugador se encuentra en la base de datos hace una petición PATCH
 # y actualiza los nuevos datos o si directamente el jugador no existe lo crea haciendo una petición POST.
-def peticionAPI(i, player_id, id_jugador, nombre_jugador, propietario, nombre_equipo, posicion, titular, partidos_jugados, ranking_general, ranking_equipo
-                , ranking_posicion, mejorFichaje, media_sofascore, media_puntos, total_puntos, puntos_buenos, ofertaMin, precio_jugador, precio_min
+def peticionAPI(id_jugador, nombre_jugador, propietario, nombre_equipo, posicion, titular, partidos_jugados, ranking_general, ranking_equipo
+                , ranking_posicion, mejorFichaje, media_sofascore, media_puntos, total_puntos, foto, ofertaMin, precio_jugador, precio_min
                 , precio_max, tarjetas_amarillas, tarjetas_rojas, tarjetas_doble_amarilla, racha, lesion):
     
     #Url con la que acceder a todos los jugadores
-    urlAPI = "http://10.228.64.253:3002/api/v1/jugadores"
+    urlAPI = "http://10.228.64.236:80/api/v3/jugadores"
 
     # El programa hace una petición a la url declarada anteriormente y recoge el contenido HTML, en el caso de se haya hecho correctamente se almacenará 
     # los identificadores de los jugadores en una lista con el nombre de "api_id". Una vez obtenidos los id, la siguiente variable guarda la estructura 
     # que tendrá el JSON con los datos que recibirá la función. Por último recoge el id de un jugador en concreto y analiza si está en la lista de los 
     # identificadores de los jugadores de la api. En caso de que esté actualiza los datos con el metodo PATCH en la petición y en el caso de que no se 
     # encuentre crea un nuevo registro para ese jugador. 
-    responseAPI = requests.get(urlAPI)
-    if responseAPI.status_code == 200:
-        data = responseAPI.json()
-        api_id = []
-        jugadores_api = data["jugadores"]
-        for j in jugadores_api:
-            api_id.append(str(j['id_jugador']))
+    
+    datosJson = {
         
-        datosJson = {
-            
-            "id": int(id_jugador),
-            "nombre": f"{nombre_jugador}",
-            "propietario": "prueba final",#f"{propietario}",
-            "equipo": f"{nombre_equipo}",
-            "posicion": f"{posicion}",
-            "titular": titular,
-            "partidos_jugados": f"{partidos_jugados}",
-            "ranking_general": ranking_general,
-            "ranking_equipo": ranking_equipo,
-            "ranking_posicion": ranking_posicion,
-            "mejor_fichaje": mejorFichaje,
-            "media_sofascore": media_sofascore,
-            "media_puntos": media_puntos,
-            "total_puntos": total_puntos,
-            "puntos_buenos": puntos_buenos,
-            "oferta_minima": ofertaMin,
-            "valor_mercado": int(precio_jugador),
-            "valor_mercado_max": precio_min,
-            "valor_mercado_min": precio_max,
-            "tarjeta_amarilla": tarjetas_amarillas,
-            "tarjeta_roja": tarjetas_rojas,
-            "doble_tarjeta_amarilla": tarjetas_doble_amarilla,
-            "racha": f"{racha}",
-            "lesion": f"{lesion}"
-            
-        }  
+        "id_jugador": int(id_jugador),
+        "nombre": f"{nombre_jugador}",
+        "propietario": f"{propietario}",
+        "equipo": f"{nombre_equipo}",
+        "posicion": f"{posicion}",
+        "titular": titular,
+        "partidos_jugados": f"{partidos_jugados}",
+        "ranking_general": ranking_general,
+        "ranking_equipo": ranking_equipo,
+        "ranking_posicion": ranking_posicion,
+        "mejor_fichaje": mejorFichaje,
+        "media_sofascore": media_sofascore,
+        "media_puntos": media_puntos,
+        "total_puntos": total_puntos,
+        "foto": foto,
+        "oferta_minima": ofertaMin,
+        "valor_mercado": int(precio_jugador),
+        "valor_mercado_max": precio_max,
+        "valor_mercado_min": precio_min,
+        "tarjeta_amarilla": tarjetas_amarillas,
+        "tarjeta_roja": tarjetas_rojas,
+        "doble_tarjeta_amarilla": tarjetas_doble_amarilla,
+        "racha": f"{racha}",
+        "lesion": f"{lesion}",
+        
+    }  
 
-        id = player_id[i-1]
+    post_response = requests.post(urlAPI, json=datosJson)
 
-        if str(id) in api_id:
+    if post_response.status_code == 200:
+        print(f"Post realizado a jugador {nombre_jugador}")
+    else:
+        print(f"Post no realizado a jugador {nombre_jugador}")
 
-            patch_response = requests.patch(urlAPI + f"/{id}/actualizar", json=datosJson)
-
-            if patch_response.status_code == 200:
-                print(f"Patch realizado a jugador {nombre_jugador}")
-            else:
-                print(f"Patch no realizado a jugador {nombre_jugador}")
-
-        else:
-            post_response = requests.post(urlAPI + "/insertar", json=datosJson)
-
-            if post_response.status_code == 200:
-                print(f"Post realizado a jugador {nombre_jugador}")
-            else:
-                print(f"Post no realizado a jugador {nombre_jugador}")
 
 
 # Declaración de variables para controlar los ficheros de texto
@@ -109,6 +91,21 @@ linea2 = ""
 cont_lineas = 0
 cont_lineas_propietario = 0
 cont_lineas_oferta = 0
+#
+#requestAPIKey = requests.post("http://10.228.64.253/api/v1/token")
+#if requestAPIKey.status_code == 200:
+#    json_data = requestAPIKey.json()
+#    apiKey = json_data.get("access_token")
+#    print("Api Key obtenida correctamente")
+#else:
+#    print("No se ha podido recoger la API Key")
+#header = {
+#    "Authorization": f"Bearer {apiKey}"
+#}
+
+
+
+
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 #
@@ -121,6 +118,7 @@ fichero_mejor.close()
 # El programa lee el fichero con el nombre e id de todos los jugadores y los guarda en dos listas
 fichero_nombres = open('web_scrapping\\input\\urls.txt','r')
 for linea in fichero_nombres:
+    linea = linea.replace("\n", "")
     split_linea = linea.split("/")
     player_name.append(split_linea[1]) 
     player_id.append(split_linea[0].replace("\n",""))
@@ -160,6 +158,7 @@ while i <= cont_lineas:
 
     # Variable que almacena la url del comuniate que irá cambiando en función del nombre e id de cada jugador
     url = "https://www.comuniate.com/jugadores/"+player_id[i-1]+"/"+player_name[i-1]
+    url = url.replace("\n", "")
 
     # Realiza la solicitud HTTP para obtener el contenido HTML
     response = requests.get(url)
@@ -178,9 +177,9 @@ while i <= cont_lineas:
         mejorFichaje = False
         if id_jugador in player_id_mejorFichaje:
              mejorFichaje = True
-        nombre_jugador = soup.select_one('#content-left > div:nth-child(1) > div.col-xs-12 > h1 > strong').get_text()
-        posicion = soup.select_one('#content-left > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div > div:nth-child(3) > strong').get_text()
-        titular = soup.select_one('#content-left > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div > div:nth-child(5) > strong').get_text()
+        nombre_jugador = soup.select_one('body > section > div.container > div:nth-child(2) > div > div:nth-child(1) > div.col-xs-12 > h1 > strong').get_text()
+        posicion = soup.select_one('body > section > div.container > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div > div:nth-child(3) > strong').get_text()
+        titular = soup.select_one('body > section > div.container > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div > div:nth-child(5) > strong').get_text()
         if(titular == "NO"):
             titular = False
         else:
@@ -194,22 +193,16 @@ while i <= cont_lineas:
         # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
         #
         # En este bloque se analiza si el jugador está fuera de la liga y si está lesionado.
-        fuera_liga = soup.select_one('#content-left > div:nth-child(1) > div.col-xs-12 > div.alert.alert-danger > strong')
+        fuera_liga = soup.select_one('body > section > div.container > div:nth-child(2) > div > div:nth-child(1) > div.col-xs-12 > div.alert.alert-danger > strong')
         if( type(fuera_liga) != NoneType ):
             fuera_liga = fuera_liga.text
             if( fuera_liga.find("ya no se encuentra")) :
                 fuera_liga = 'SI'
             else :
                 fuera_liga = 'NO'
-        lesionado = soup.select_one('#content-left > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div.col-xs-12 > div')
+        lesionado = soup.select_one('body > section > div.container > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div.col-xs-12 > div')
         lesion = "NO"
         if( type(lesionado) != NoneType ):
-            posicion = soup.select_one('#content-left > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div > div:nth-child(3) > strong').get_text()
-            titular = soup.select_one('#content-left > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div > div:nth-child(5) > strong').get_text()
-            if(titular == "NO"):
-                titular = False
-            else:
-                titular = True 
             if( not lesionado.text.__contains__("FUERA DE LA LISTA")):
                 lesion = lesionado.text
         #
@@ -222,19 +215,19 @@ while i <= cont_lineas:
         #
         # En este bloque se recogen los datos de la media de todos los puntos que ha hecho el jugador, la media de puntos de SofaScore, el equipo al cuál 
         # pertenece actualmente, el total de puntos realizados, el número de tarjetas (amarillas, doble amarillas y rojas) y por último los partidos jugados
-        media_puntos = float(soup.select_one('#content-left > div:nth-child(2) > div:nth-child(1) > div.row.fondo_stats > div:nth-child(2) > div > span:nth-child(1) > strong').get_text())
-        media_sofascore= soup.select_one('#content-left > div:nth-child(2) > div:nth-child(1) > div.row.fondo_stats > div:nth-child(3) > div > span:nth-child(1) > strong')
+        media_puntos = float(soup.select_one('body > section > div.container > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div.row.fondo_stats > div:nth-child(2) > div > span:nth-child(1) > strong').get_text())
+        media_sofascore= soup.select_one('body > section > div.container > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div.row.fondo_stats > div:nth-child(3) > div > span:nth-child(1) > strong')
         if( media_sofascore.get_text() != '' ):
             media_sofascore = float(media_sofascore.get_text())
         else:
             media_sofascore = 0
-        equipo = soup.select_one('#content-left > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > img:nth-child(1)')
+        equipo = soup.select_one('body > section > div.container > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > img:nth-child(1)')
         nombre_equipo = equipo['title']
-        total_puntos = int(soup.select_one("#content-left > div:nth-child(2) > div:nth-child(1) > div.row.fondo_stats > div:nth-child(1) > div > span:nth-child(1) > strong").get_text())
-        tarjetas_amarillas = int(soup.select_one('#content-left > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > span:nth-child(5)').get_text())
-        tarjetas_rojas = int(soup.select_one('#content-left > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > span:nth-child(9)').get_text())
-        tarjetas_doble_amarilla = int(soup.select_one('#content-left > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > span:nth-child(7)').get_text())
-        partidos_jugados = soup.select_one("#content-left > div:nth-child(2) > div:nth-child(1) > div.row.fondo_stats > div:nth-child(4) > div > span:nth-child(1) > strong").get_text()
+        total_puntos = int(soup.select_one("body > section > div.container > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div.row.fondo_stats > div:nth-child(1) > div > span:nth-child(1) > strong").get_text())
+        tarjetas_amarillas = int(soup.select_one('body > section > div.container > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > span:nth-child(5)').get_text())
+        tarjetas_rojas = int(soup.select_one('body > section > div.container > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > span:nth-child(9)').get_text())
+        tarjetas_doble_amarilla = int(soup.select_one('body > section > div.container > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > span:nth-child(7)').get_text())
+        partidos_jugados = soup.select_one("body > section > div.container > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div.row.fondo_stats > div:nth-child(4) > div > span:nth-child(1) > strong").get_text()
         #
         # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
@@ -245,9 +238,9 @@ while i <= cont_lineas:
         #
         # Este bloque guarda la racha que lleva el jugador, el precio máximo que ha alcanzado y el mínimo alcanzado.
         racha = ""
-        precio_max = int(soup.select_one('#content-left > div:nth-child(2) > div:nth-child(8) > div:nth-child(2) > span').get_text().replace("€","").replace(".",""))
-        precio_min = int(soup.select_one('#content-left > div:nth-child(2) > div:nth-child(8) > div:nth-child(3) > span').get_text().replace("€","").replace(".",""))
-        racha_flecha = soup.select_one('#content-left > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div > div:nth-child(1) > span > i')
+        precio_max = int(soup.select_one('body > section > div.container > div:nth-child(2) > div > div:nth-child(4) > div.col-md-8 > div:nth-child(4) > div:nth-child(2) > span').get_text().replace("€","").replace(".",""))
+        precio_min = int(soup.select_one('body > section > div.container > div:nth-child(2) > div > div:nth-child(4) > div.col-md-8 > div:nth-child(4) > div:nth-child(3) > span').get_text().replace("€","").replace(".",""))
+        racha_flecha = soup.select_one('body > section > div.container > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div > div:nth-child(1) > span > i')
         racha_class = racha_flecha.get('class')
         if "fa-arrow-up" in racha_class:
            racha = "Buena"
@@ -255,6 +248,8 @@ while i <= cont_lineas:
             racha = "Normal"
         elif "fa-arrow-down" in racha_class:
             racha = "Mala"
+
+        foto = soup.select_one('body > section > div.container > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > img:nth-child(2)').get('src')
         #
         # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
@@ -272,7 +267,7 @@ while i <= cont_lineas:
              for h in player_id_oferta:
                 if id_jugador in h :
                     index_oferta = player_id_oferta.index(id_jugador)
-                    ofertaMin = oferta[index_oferta]
+                    ofertaMin = int(oferta[index_oferta])
                     break
                 else:
                  linea2 +=1
@@ -290,8 +285,6 @@ while i <= cont_lineas:
             propietario = propietarios[linea]
         #
         # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-
 
 
         # Ahora los siguientes datos se sacarán del apartado de mercado de cada jugador
@@ -331,16 +324,11 @@ while i <= cont_lineas:
         # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     
 
-        # En esta variable se pretendía guardar la suma de los puntos de cada jornada que supere el numero 5, la cosa es que al intentar recoger los puntos
-        # por culpa de la estructura del html era imposible acceder a estos despúes de haberlo intentado de diversas formas. Por temas de organización de la
-        # base de datos no se podía quitar este campo así que siempre será null.    
-        puntos_buenos = None
-        
-
         # Una vez recogidos todos los datos para almacenar en formato json se llama a la función
-        peticionAPI(i, player_id, id_jugador, nombre_jugador, propietario, nombre_equipo, posicion, titular, partidos_jugados, ranking_general, ranking_equipo
-        , ranking_posicion, mejorFichaje, media_sofascore, media_puntos, total_puntos, puntos_buenos, ofertaMin, precio_jugador, precio_min, precio_max
+        peticionAPI(id_jugador, nombre_jugador, propietario, nombre_equipo, posicion, titular, partidos_jugados, ranking_general, ranking_equipo
+        , ranking_posicion, mejorFichaje, media_sofascore, media_puntos, total_puntos, foto, ofertaMin, precio_jugador, precio_min, precio_max
         , tarjetas_amarillas, tarjetas_rojas, tarjetas_doble_amarilla, racha, lesion)
+
 
     
     # En el caso que no se haya podido acceder al Comuniate mostraría un codigo de error por consola
@@ -351,7 +339,7 @@ while i <= cont_lineas:
     i+=1
 
 
-
+    
 # Una vez que se hayan hecho los cambios en todos los jugadores, se borran los siguientes ficheros para que no se repita la información
 os.remove("web_scrapping/input/mejorFichaje.txt")
 os.remove("web_scrapping/input/OfertaMinima.txt")
